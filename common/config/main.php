65c98e6d5a016bc7b237d08d4a25c4f5b5b02cfb
CloverWeb <370546391@qq.com>
2015-12-15 19:02:00 +0800

    修改urlManage路由格式（url路径格式），数据库配置，删除 index.php 无用文件

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,35 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+
+            ],
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.163.com',
+                'username' => '',
+                'password' => '',
+                'port' => '25',
+                'encryption' => 'tls',
+            ],
+
+            'messageConfig'=>[
+                'charset'=>'UTF-8',
+                'from'=>['邮箱@163.com'=>'发送人名称']
+            ],
+        ],
+
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2015/12/14 0014
- * Time: 下午 5:22
- */
